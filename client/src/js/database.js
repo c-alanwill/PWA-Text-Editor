601cf31e6a2813a9d56f0f9f030e4d11f09a766b
Cory Williams <corywilliams@me.com>
2023-01-08 16:32:03 -0800

    fixes to database file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,16 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 
-  // is line 18 correct or do I need id, content?
 export const putDb = async (content) =>{
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-
-
-  // is line 26 todo correct?
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: 1, todo: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
